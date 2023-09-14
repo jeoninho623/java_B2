e70d347a01336004c6d4a187cf3cb62776f7bf74
jeoninho623 <0623inho@naver.com>
2023-09-14 17:25:20 +0900

    자바

--- day08/src/exam03/Ex_Book.java
+++ day08/src/exam03/Ex_Book.java
@@ -3,7 +3,7 @@ package exam03;
 public class Ex_Book {
     public static void main(String[] args) {
         Book b1 = new Book("책1", "저자1","출판사1");
-        System.out.println(b1);
+        System.out.println(b1);     // Object::toString();
 
     }
 }
---
+++ day08/src/exam03/Ex_ClassClass01.java
@@ -0,0 +1,7 @@
+package exam03;
+
+public class Ex_ClassClass01 {
+    public static void main(String[] args) throws ClassNotFoundException{
+        Class cls = Class.forName("exam03.Book");
+    }
+}
