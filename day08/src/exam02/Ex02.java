db7fe20a33291aebc69641781ae670d5c2ab15bd
tlschdaud <tlschdaud1@naver.com>
2023-09-14 16:17:22 +0900

    자바

---
+++ day08/src/exam02/Ex02.java
@@ -0,0 +1,15 @@
+package exam02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        //Human human = new Human();
+        //Bird bird = new Bird();
+        //Tiger tiger = new Tiger();
+
+        Object[] animals = {
+                new Human(),
+                new Tiger(),
+                new Bird()
+        };
+    }
+}
